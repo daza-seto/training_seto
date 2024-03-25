2763579359f086ef5072f20cd578807e0fbee4be
daza-seto <d_seto@ucsb.edu>
2024-03-25 12:37:51 -0700

    retry

--- intro-to-programming.R
+++ intro-to-programming.R
@@ -1,15 +1,6 @@
-weight_kg <- c(55, 25, 12)
-
-#convert weight to lbs
-2.2 * weight_kg
-
-science_rocks <-"yes, it does!"
-
-#mean of weight_kg
-mean_weight_kg <- mean(x = weight_kg)
 
 #read in data ----
-bg_chem_dat <- read.csv("data/BGchem2008data.csv")
+well_dat <- read.csv("data/BGchem2008data.csv")
 
 #Using the subset operator '$'
 head(bg_chem_dat$Date)
